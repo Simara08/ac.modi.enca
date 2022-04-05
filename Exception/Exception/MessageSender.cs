9e238cfe0a20c046ba78541f44fb63bb4380071e
Simara08 <100687456+Simara08@users.noreply.github.com>
2022-04-06 00:16:50 +0400

    Add files via upload

---
+++ Exception/Exception/MessageSender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptionn
+{
+    public abstract class MessageSender
+    {
+        public abstract void SendMessage();
+       
+    }
+    public  class EmailSender : MessageSender
+    {
+        public  override void SendMessage()
+        {
+            Console.WriteLine("Send message via email");
+        }
+    }
+    public class SmsSender : MessageSender
+    {
+        public override void SendMessage()
+        {
+            Console.WriteLine("Send message via sms");
+        }
+    }
+}
---
+++ Exception/Exception/Program.cs
@@ -0,0 +1,27 @@
+﻿using Exceptionn;
+using System;
+
+namespace Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1-Email ilə mesaj göndərilməlidi.\n2-Sms ilə mesaj göndərilməlidi.\n0-Quit");
+            int Choice = Int32.Parse(Console.ReadLine());
+            EmailSender byemail = new EmailSender();
+            SmsSender bysms = new SmsSender();
+            switch (Choice)
+            {
+                case 1:byemail.SendMessage();
+                    break;
+                case 2:bysms.SendMessage();
+                    break;
+                case 3:Environment.Exit(1);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}

2de34271d0a8c0a261e30e9ec151587971ee850a
Simara08 <100687456+Simara08@users.noreply.github.com>
2022-04-09 23:45:21 +0400

    Add files via upload

---
+++ ExceptionMethods/ExceptionMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExceptionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 7;
+            string Wordd = "how are you baby";
+            
+            Console.WriteLine(a.IsPrime());
+            Console.WriteLine(Wordd.CustomContains("are"));
+        }
+    }
+}

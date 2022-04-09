3df4b70b4f78278323008a98f45ce26737deec0d
Simara08 <100687456+Simara08@users.noreply.github.com>
2022-04-09 23:41:31 +0400

    Add files via upload

---
+++ Mylistt/ExceptionMethods/ExceptionMethods/Exceptionn.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionMethods
+{
+     public static  class Exceptionn
+    {
+
+        public static bool CustomContains(this string str,string word)
+        {
+            if (str.Contains(word))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool CustomContains(this string str, char w)
+        {
+            if (str.Contains(w))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static  bool IsPrime(this int num)
+        {
+            int i;
+            for ( i = 2; i < num; i++)
+            {
+                if (num%i==0)
+                {
+                    return false;
+                }
+            }
+            if (i==num)
+            {
+                return true;
+
+            }
+            return false;
+        }
+    }
+}
